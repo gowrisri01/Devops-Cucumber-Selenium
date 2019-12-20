8f3575ae327a571f7045265594766e2ebb110e21
gowrisri01 <59052097+gowrisri01@users.noreply.github.com>
2019-12-20 15:13:42 +0530

    Add files via upload

---
+++ src/test/java/skeleton/loginfile.java
@@ -0,0 +1,39 @@
+package skeleton;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import cucumber.api.java.en.Given;
+
+public class loginfile {
+	WebDriver driver;
+
+	@Given("Launch the chrome borwser")
+	public void launch_the_chrome_borwser() throws Exception {
+		 System.setProperty("webdriver.chrome.driver", "C:\\C2C.04.02_Softwares\\chromedriver.exe");
+		  driver=new ChromeDriver();
+		  driver.get("http://newtours.demouat.com");
+		  //Thread.sleep(2000);
+	}
+	
+	@Given("enter valid uname and pwd")
+	public void enter_valid_uname_and_pwd() {
+		driver.findElement(By.name("username")).sendKeys("mercury");;
+		driver.findElement(By.name("password")).sendKeys("mercury");;
+	}
+
+	@Given("Click submit button")
+	public void click_submit_button() {
+		driver.findElement(By.name("login")).click();
+	}
+
+	@Given("close the browser")
+	public void close_the_browser() {
+	    driver.close();
+	}
+
+	 
+}
+
---
+++ src/test/resources/skeleton/login.feature
@@ -0,0 +1,7 @@
+Feature: Login
+
+  Scenario: login with valid input
+    * Launch the chrome borwser
+    * enter valid uname and pwd
+    * Click submit button
+    * close the browser
